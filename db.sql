986aba7f6ff0bb9c7945cf34252cfd8fa6f98500
Driwsk <driwsk2003@gmail.com>
2024-03-14 21:34:04 -0300

    segund commit

--- db.sql
+++ db.sql
@@ -1,11 +1,59 @@
-import { Sequelize } from 'sequelize';
-import SQLite from 'sqlite3';
+--use dbcadprod;
 
-const sequelize = new Sequelize('database', 'username', 'password', {
-  dialect: 'sqlite',
-  storage: 'path/to/database.sqlite', // or ':memory:'
-  dialectOptions: {
-    
-    mode: SQLite.OPEN_READWRITE | SQLite.OPEN_CREATE | SQLite.OPEN_FULLMUTEX,
-  },
-});
+CREATE TABLE pessoas
+(
+    id INT PRIMARY KEY IDENTITY(cpf),
+    nome VARCHAR(50),
+    idade INT(2),
+    cpf INT(11),
+    email VARCHAR(),
+    stst VARCHAR()
+);
+
+
+
+
+--criação de tabelas
+/*CREATE TABLE marcas
+(
+id INT PRIMARY KEY IDENTITY(1,1),
+descricao VARCHAR (50),
+tipo VARCHAR(2)
+);
+
+CREATE TABLE produto
+(
+id INT PRIMARY KEY IDENTITY(1,1),
+marcas_id INT,
+nome VARCHAR(100),
+preco MONEY,
+fotos varchar(50)
+);
+
+--criação de relacionamentos
+ALTER TABLE produto
+ADD CONSTRAINT fk_marcas_id
+FOREIGN KEY (marcas_id)
+REFERENCES marcas(id);
+
+--inserção de dados
+INSERT marcas
+(descricao,tipo)
+VALUES
+('Broto legal','A'),
+('Prato fino','A'),
+('Carmil','B'),
+('Scala','B');
+
+INSERT produto
+(marcas_id,nome,preco,fotos)
+VALUES
+(1,'Feijao',3,'fbl.png'),
+(2,'Arroz Sinha',5,'fbl.png'),
+(3,'Arroz Dede',5,'fbl.png'),
+(4,'Parafuso',3,'fbl.png');
+
+
+
+select * from marcas;
+select * from produto;*/
